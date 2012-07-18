b3f3680ff36aac455e1ebccabfb17ee7158f4431
Makarovich <hobbarobot@live.com>
2012-07-18 03:20:28 -0400

    Built Folder Struct.
    I also built the documentation, Thanks Phil for DBAD!

---
+++ Documentation/Documentation.php
@@ -0,0 +1,29 @@
+<?php
+	/*
+		RaGEWEB :: Web Application used to emulator the current build of Habbo.com
+		Developers
+			- Cobe 'Makarov' Johnson :: Project Team Leader, designed database, folder structure, reviews code before github push, etc.
+			- Ashley 'nobrain' Davidson :: Writes code, rips public files.
+			- Leon Hartley :: Writes code, designs layout.
+
+		Project protected under the DBAD License
+
+		DBAD Information
+			- Creator :: Phil Sturgeon
+			- Created :: December, 2009
+			- Local Location :: \Documentation\DBAD.txt
+
+		RaGEWEB Information
+			- Started :: July 18, 2012
+			- Languages Used :: PHP, Javascript, CSS
+			- Code Pattern :: MVC
+			- Extensions Used :: APC
+
+		File Information
+			- Name :: 
+			- Location ::
+			- Author :: 
+			- Description ::
+			- Created :: --2012
+	*/
+?>
